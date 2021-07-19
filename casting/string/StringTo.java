4cdcaf3daa87585b88cbd15c800d8e021f4f577d
ads0070 <gch05610@naver.com>
2021-07-20 01:32:04 +0900

    Add Add method to cast string to charArray

--- casting/string/StringTo.java
+++ casting/string/StringTo.java
@@ -1,9 +1,26 @@
 package string;
 
 public class StringTo {
+	
 	public int convert_StringToInteger(String str) {
 		int num = Integer.parseInt(str);
 		
 		return num;
 	}
+	
+	public String[] convert_StringToStringArray(String str) {
+		String[] str_arr = str.split("");
+		
+		return str_arr;
+	}
+	
+	public char[] convert_StringToCharArray(String str) {
+		char[] char_arr = {};
+		
+		char_arr = str.toCharArray();
+		
+		return char_arr;
+	}
+	
+	
 }
