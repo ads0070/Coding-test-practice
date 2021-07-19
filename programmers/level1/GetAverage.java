be83f3bf1ba5af7ff810b9cb2b53e4020a1c0be7
ads0070 <gch05610@naver.com>
2021-07-20 00:37:42 +0900

    연습문제 > 평균 구하기

---
+++ programmers/level1/GetAverage.java
@@ -0,0 +1,17 @@
+package level1;
+
+// Æò±Õ ±¸ÇÏ±â
+public class GetAverage {
+	public double solution(int[] arr) {
+        double answer = 0;
+        double sum = 0;
+        
+        for (int n : arr) {
+        	sum += n;
+        }
+        
+        answer = sum / arr.length;
+        
+        return answer;
+    }
+}
