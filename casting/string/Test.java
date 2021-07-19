e47a02e403bec78794110dfac578e504fddde1ee
ads0070 <gch05610@naver.com>
2021-07-20 01:13:23 +0900

    test

---
+++ casting/string/Test.java
@@ -0,0 +1,9 @@
+package string;
+
+public class Test {
+	public static void main(String[] args) {
+		StringTo sti = new StringTo();
+		String s = "38392";
+		System.out.println("int : "+sti.casting_StringToInteger(s));
+	}
+}
