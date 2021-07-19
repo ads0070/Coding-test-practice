874489d1610e8ed53c6bed09802cb1bb585a041e
ads0070 <gch05610@naver.com>
2021-07-20 01:16:06 +0900

    Add method to cast string to integer

--- casting/string/StringTo.java
+++ casting/string/StringTo.java
@@ -1,7 +1,7 @@
 package string;
 
 public class StringTo {
-	public int casting_StringToInteger(String str) {
+	public int convert_StringToInteger(String str) {
 		int num = Integer.parseInt(str);
 		
 		return num;
