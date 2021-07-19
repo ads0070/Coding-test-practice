9cee4f8bbe5ffc547835ba4704de72d42333393b
ads0070 <gch05610@naver.com>
2021-07-20 01:31:10 +0900

    test

--- casting/string/Test.java
+++ casting/string/Test.java
@@ -1,9 +1,21 @@
 package string;
 
+import java.util.Arrays;
+
 public class Test {
 	public static void main(String[] args) {
-		StringTo sti = new StringTo();
-		String s = "38392";
-		System.out.println("int : "+sti.convert_StringToInteger(s));
+		StringTo st = new StringTo();
+		String str_num = "38392";
+		String str = "Hello";
+		
+		// String To Integer
+		System.out.println("String : " + str_num + " | int : " + st.convert_StringToInteger(str_num));
+		
+		// String to StringArray
+		System.out.println("String : " + str + " | StringArray : "+Arrays.toString(st.convert_StringToStringArray(str)));
+		
+		// String to CharArray
+		System.out.println("String : " + str + " | charArray : "+Arrays.toString(st.convert_StringToCharArray(str)));
+		
 	}
 }
