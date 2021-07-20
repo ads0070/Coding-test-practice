aa36c3005ee904c5c8ccc2ebe4fee3b1b775af03
ads0070 <gch05610@naver.com>
2021-07-20 23:27:26 +0900

    연습문제 > 짝수와 홀수

---
+++ programmers/level1/EvenAndOdd.java
@@ -0,0 +1,12 @@
+package level1;
+
+// Â¦¼ö¿Í È¦¼ö
+public class EvenAndOdd {
+    public String solution(int num) {
+        String answer = "";
+
+        answer = (num%2==0) ? "Even" : "Odd";
+
+        return answer;
+    }
+}
