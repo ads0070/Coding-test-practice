e70bbc5179265717a217bdd1f14834f03a5a6844
ads0070 <gch05610@naver.com>
2021-07-22 22:03:43 +0900

    test

--- programmers/level2/test.java
+++ programmers/level2/test.java
@@ -4,6 +4,15 @@ public class test {
 
     public static void main(String[] args) {
 
+        // «¡∏∞≈Õ
+        /*Printer p = new Printer();
+        int[] priorities = {1, 1, 9, 1, 1, 1};
+        int loc = 0;
+        System.out.println(p.solution(priorities, loc));*/
+
+
+
+
     }
 
 }
