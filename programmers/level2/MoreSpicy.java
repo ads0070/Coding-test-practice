dc06e186a0d762baec3a16c34ef086f99c6d4345
ads0070 <gch05610@naver.com>
2021-07-24 21:23:21 +0900

    timeout : need to understand the heap and modify code

--- programmers/level2/MoreSpicy.java
+++ programmers/level2/MoreSpicy.java
@@ -4,61 +4,42 @@ package level2;
 import java.util.ArrayList;
 
 public class MoreSpicy {
-    private ArrayList<Integer> heap = new ArrayList<>() {
-        {add(0);}
-    };
+    private final ArrayList<Integer> heap = new ArrayList<>();
 
     public int solution(int[] scoville, int K) {
         int answer = 0;
 
-        for (int i : scoville) {
-            insert(i);
-        }
+        for(int i : scoville) heap.add(i);
 
-        System.out.println(heap);
-        System.out.println(combine());
-        System.out.println(heap);
+        heap_sort();
+
+        while (heap.get(0) < K) {
+            if(heap.size() <= 1) return -1;
+
+            heap.add(heap.get(0) + (heap.get(1) * 2));
+            heap.remove(0);
+            heap.remove(0);
+
+            heap_sort();
+            answer++;
+        }
 
         return answer;
     }
 
-    private void insert(int n) {
-        heap.add(n);
-        int p = heap.size() - 1;
-
-        while (p > 1 && heap.get(p/2) > heap.get(p)) {
-            int temp = heap.get(p/2);
-            heap.set(p/2, heap.get(p));
-            heap.set(heap.get(p/2), temp);
-
-            p = p / 2;
+    private void heap_sort() {
+        for(int i = 1; i < heap.size(); i++) {
+            int p = i;
+            do {
+                int root = p / 2;
+                if(heap.get(root) > heap.get(p)) {
+                    int temp = heap.get(root);
+                    heap.set(root, heap.get(p));
+                    heap.set(p, temp);
+                }
+                p = root;
+            } while (p != 0);
         }
-    }
-
-    private int combine() {
-        int minScoville = heap.get(1);
-
-        heap.set(1, heap.get(heap.size()-1));
-        heap.remove(heap.size()-1);
-
-        int pos = 1;
-        while((pos*2) < heap.size()) {
-            int min = heap.get(pos * 2);
-            int minPos = pos * 2;
-
-            if (((pos*2 + 1) < heap.size()) && (min > heap.get(pos*2+1))) {
-                min = heap.get(pos*2+1);
-                minPos = pos * 2 + 1;
-            }
-            if (heap.get(pos) < min) break;
-
-            int temp = heap.get(pos);
-            heap.set(pos, heap.get(minPos));
-            heap.set(minPos, temp);
-            pos = minPos;
-        }
-
-        return minScoville;
     }
 
 }
